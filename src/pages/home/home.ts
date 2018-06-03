af96d3cbb0b18dfcf5f9e3734fce71b87362f742
Luis Garcia <luisgr01@gmail.com>
2018-06-03 20:34:31 +0200

    added storage feature for when it is offline

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -4,8 +4,7 @@ import { RssProvider } from '../../providers/rss/rss';
 import { FeedItem } from '../../models/FeedItem';
 import { ArrayUtilProvider } from '../../providers/array-util/array-util';
 import { FeedDetailPage } from '../feed-detail/feed-detail';
-
-
+import { StorageProvider } from '../../providers/storage/storage';
 
 
 @IonicPage()
@@ -17,10 +16,13 @@ export class HomePage {
 
   private rssData: Array<FeedItem>;
 
+  private readonly storageKey = 'feedItems';
+
   constructor(public navCtrl: NavController, 
               public navParams: NavParams,
               public rssProvider: RssProvider,
-              public arrayUtilProvider: ArrayUtilProvider) {
+              public arrayUtilProvider: ArrayUtilProvider,
+              public storageProvider: StorageProvider) {
   }
 
   ionViewDidLoad() {
@@ -32,10 +34,16 @@ export class HomePage {
       data => {
         //se obtienen los datos y se ordenan por fecha de manera inversa (de más reciente a más lejana)
         this.rssData = this.arrayUtilProvider.sortItems(data, "date", true); 
+        this.storageProvider.saveData(this.storageKey, this.rssData);
         console.log(data);
       },
       error => {
         console.log(<any>error);
+        this.storageProvider.getDataByKey(this.storageKey).then(
+          feedItems => {
+            this.rssData = feedItems;
+          }
+        );
       }
     );
   }
